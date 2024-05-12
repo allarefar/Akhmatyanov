d444939e7616ec14f9386f3fbd781492a5f3eeee
marsel_akhmatyanov <makhmatyanov@inbox.ru>
2024-05-12 22:53:42 +0500

    1.2

---
+++ app/src/main/java/com/example/ahmatynov/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ahmatynov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val btnNavView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val controller=findNavController(R.id.fragmentContainerView)
+        btnNavView.setupWithNavController(controller)
+
+    }
+}
---
+++ app/src/main/java/com/example/ahmatynov/User.kt
@@ -0,0 +1,4 @@
+package com.example.ahmatynov
+
+class User(val login: String, val email: String, val pass: String, ) {
+}
